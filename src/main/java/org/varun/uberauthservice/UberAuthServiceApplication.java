cdefe3b17702d74e7971eb6d47883edbf42e39ff
Varun Gupta <kumarvarun5656@gmail.com>
2026-05-05 08:27:55 +0530

    just setting up

---
+++ src/main/java/org/varun/uberauthservice/UberAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package org.varun.uberauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberAuthServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/varun/uberauthservice/UberAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.varun.uberauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
