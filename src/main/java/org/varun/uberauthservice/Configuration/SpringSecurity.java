b20afe93db93b1fd758cd8ed6c6d116a692b92ac
Varun Gupta <kumarvarun5656@gmail.com>
2026-05-09 18:50:02 +0530

    Jwt creation and Added payload extraction methods

--- src/main/java/org/varun/uberauthservice/Configuration/SpringSecurity.java
+++ src/main/java/org/varun/uberauthservice/Configuration/SpringSecurity.java
@@ -8,7 +8,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
+
 public class SpringSecurity {
 
 
